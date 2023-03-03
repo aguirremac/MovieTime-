c2b90075db68d6332d574a9bf09157d3615fdcdc
magagr17 <markjohnaguirre@gmail.com>
2023-03-03 13:17:06 +0800

    try fix removed @

--- layouts/movie.vue
+++ layouts/movie.vue
@@ -4,12 +4,12 @@
         
         <!-- blurred background poster -->
         <div class="w-full h-full absolute top-0 left-0 z-10">
-            <div  class="h-full blur-150px"></div>
+            <div bg-movie class=" bg-movie h-full blur-150px"></div>
         </div>
         <!-- nav bar -->
         <div class="w-1440px h-screen z-30">
             <nav class="flex w-full justify-between  h-47px my-41px items-center">
-                <nuxt-link to="/"><img src="@/public/arrow-back-rounded.svg" class="text-white h-34px cursor-pointer"/></nuxt-link>
+                <nuxt-link to="/"><img src="/public/arrow-back-rounded.svg" class="text-white h-34px cursor-pointer"/></nuxt-link>
                 <div class="flex text-16px font-semibold gap-60px pl-50px">
                     <nuxt-link class="format tracking-widest cursor-pointer router-link-active" to="/">MOVIES</nuxt-link>
                     <nuxt-link class="format tracking-widest cursor-pointer" to="/theaters">THEATERS</nuxt-link>
@@ -20,14 +20,14 @@
       <div class="flex gap-x-47px mt-31px">
             <!-- left side  -->
             <div class="flex flex-col justify-start">
-                <img src="@/images/doctor.png" alt="poster" class="mb-18px h-398px w-268px">
+                <img src="/images/doctor.png" alt="poster" class="mb-18px h-398px w-268px">
                 <div class="flex justify-center items-center mb-11px h-74px w-268px bg-#030303 rounded-lg">
-                    <img src="@/public/imdb.png" alt="" class="h-35px w-64px pr-52px ">
-                    <img src="@/public/star.png" alt="" class="h-22px pr-11px">
+                    <img src="/imdb.png" alt="" class="h-35px w-64px pr-52px ">
+                    <img src="/star.png" alt="" class="h-22px pr-11px">
                     <h1 class="text-31px font-semibold">7.4</h1>
                 </div>
                 <div class="flex justify-center items-center h-74px w-268px bg-#030303 rounded-lg">
-                    <img src="@/public/rotten.png" alt="" class="h-40px pr-64px">
+                    <img src="/rotten.png" alt="" class="h-40px pr-64px">
                     <h1 class="text-31px font-semibold">83%</h1>
                 </div>
             </div>
@@ -80,11 +80,11 @@
     
 }
 
-/* .bg-movie {
+.bg-movie {
     background-image: url('/images/doctor.png');
     background-position: 50% 35%;
     background-size: cover;
-  } */
+  }
 
 </style>
 
