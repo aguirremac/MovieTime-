0b404cc1677379d5c1f26566b79aad9ea9a5e573
magagr17 <markjohnaguirre@gmail.com>
2023-03-03 16:10:25 +0800

    added dynamic bg features

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,9 @@
 
           <div class="flex flex-wrap gap-x-20px gap-y-17px">
             <!-- mapped cards -->
-              <nuxt-link v-for="(movie, index) in movies" :key="index" :to="{ path: '/movie/' + movie.title.toLowerCase().replace(/ /g, '-') }" class="w-240px h-356px  cursor-pointer hover:scale-105 duration-200 overflow-hidden">
+              <nuxt-link  v-for="(movie, index) in movies" 
+                          :key="index"
+                          :to="{ path: '/movie/' + movie.title.toLowerCase().replace(/ /g, '-') }" class="w-240px h-356px  cursor-pointer hover:scale-105 duration-200 overflow-hidden">
                   <img :src="'/images/' + movie.posterLink" :alt="movie.title" class="w-full object-cover "/>
               </nuxt-link>
               
@@ -21,155 +23,23 @@
   </div>
 </template>
 
+
+
 <script>
 
+const {movies} = useMovieData();
+
 export default {
-
-data() {
-  return {
-    movies: [
-    {
-      
-      title: "Doctor Strange in the Multiverse of Madness",
-      movieId : "movie1",
-      year: "2022",
-      rated: "PG-13",
-      released: "18 Dec 2022",
-      runtime: "2h 6m",
-      imdbRating: "6.9",
-      rTRating: "74%",
-      posterLink: "doctor.png",
-      type: "movie",
-    },
-    {
-      
-      title: "The Bad Guys",
-      movieId : "movie2",
-      year: "2022",
-      rated: "PG",
-      released: "22 April 2022",
-      runtime: "1h 40m",
-      imdbRating: "6.8",
-      rTRating: "62%",
-      posterLink: "bad.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Sonic the HedgeHog 2",
-      movieId : "movie3",
-      year: "2022",
-      rated: "PG-13",
-      released: "8 April 2022",
-      runtime: "2h 2m",
-      imdbRating: "6.5",
-      rTRating: "64%",
-      posterLink: "sonic.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Firestarter",
-      movieId : "movie4",
-      year: "2022",
-      rated: "R",
-      released: "12 May 2022",
-      runtime: "1h 34m",
-      imdbRating: "4.6",
-      rTRating: "55%",
-      posterLink: "firestarter.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Everything Everywhere All at Once",
-      movieId : "movie5",
-      year: "2022",
-      rated: "R",
-      released: "8 April 2022",
-      runtime: "2h 19m",
-      imdbRating: "8.0",
-      rTRating: "85%",
-      posterLink: "eeaao.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Doctor Strange in the Multiverse of Madness",
-      movieId : "movie1",
-      year: "2022",
-      rated: "PG-13",
-      released: "18 Dec 2022",
-      runtime: "2h 6m",
-      imdbRating: "6.9",
-      rTRating: "74%",
-      posterLink: "doctor.png",
-      type: "movie",
-    },
-    {
-      
-      title: "The Bad Guys",
-      movieId : "movie2",
-      year: "2022",
-      rated: "PG",
-      released: "22 April 2022",
-      runtime: "1h 40m",
-      imdbRating: "6.8",
-      rTRating: "62%",
-      posterLink: "bad.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Sonic the HedgeHog 2",
-      movieId : "movie3",
-      year: "2022",
-      rated: "PG-13",
-      released: "8 April 2022",
-      runtime: "2h 2m",
-      imdbRating: "6.5",
-      rTRating: "64%",
-      posterLink: "sonic.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Firestarter",
-      movieId : "movie4",
-      year: "2022",
-      rated: "R",
-      released: "12 May 2022",
-      runtime: "1h 34m",
-      imdbRating: "4.6",
-      rTRating: "55%",
-      posterLink: "firestarter.png",
-      type: "movie",
-    },
-    {
-      
-      title: "Everything Everywhere All at Once",
-      movieId : "movie5",
-      year: "2022",
-      rated: "R",
-      released: "8 April 2022",
-      runtime: "2h 19m",
-      imdbRating: "8.0",
-      rTRating: "85%",
-      posterLink: "eeaao.png",
-      type: "movie",
-    },
-    
-  ]
-
-        }},
-
-  methods: {
+  data(){
+return {
+  movies: movies
+}},
+methods: {
       redirectToPage (index){
       const slug = movies[index].title.toLowerCase().replace(/ /g, '-')
       this.$router.push(`/movie/${slug}`)
     }
   },
-
 }
 
 </script>
--- pages/movie/[id].vue
+++ pages/movie/[id].vue
@@ -1,16 +1,101 @@
 <template>
-    <div>
+    
+    <div class=" w-full text-#FFFFFF bg-gradient-to-br from-#000000 to-#575757 flex justify-center">
+        
+        <!-- blurred background poster -->
+        <div  class="w-full h-full absolute top-0 left-0 z-10">
+            <div :style="bgStyle" class=" bg-movie h-full blur-150px"></div>
+        </div>
+        <!-- nav bar -->
+        <div class="w-1440px h-screen z-30">
+            <nav class="flex w-full justify-between  h-47px my-41px items-center">
+                <nuxt-link to="/"><img src="/arrow-back-rounded.svg" class="text-white h-34px cursor-pointer"/></nuxt-link>
+                <div class="flex text-16px font-semibold gap-60px pl-50px">
+                    <nuxt-link class="format tracking-widest cursor-pointer router-link-active" to="/">MOVIES</nuxt-link>
+                    <nuxt-link class="format tracking-widest cursor-pointer" to="/theaters">THEATERS</nuxt-link>
+                </div>
+             </nav>
 
+      <!-- main content -->
+      <div  class="flex gap-x-47px mt-31px">
+            <!-- left side  -->
+            <div class="flex flex-col justify-start">
+                <img :src="'/images/' + selectedMovie.posterLink"  alt="poster" class="mb-18px h-398px w-268px">
+                <div class="flex justify-center items-center mb-11px h-74px w-268px bg-#030303 rounded-lg">
+                    <img src="/imdb.png" alt="" class="h-35px w-64px pr-52px ">
+                    <img src="/star.png" alt="" class="h-22px pr-11px">
+                    <h1 class="text-31px font-semibold">7.4</h1>
+                </div>
+                <div class="flex justify-center items-center h-74px w-268px bg-#030303 rounded-lg">
+                    <img src="/rotten.png" alt="" class="h-40px pr-64px">
+                    <h1 class="text-31px font-semibold">83%</h1>
+                </div>
+            </div>
+
+
+            <!-- right side -->
+            <div class="h-full flex flex-col justify-start pt-8px">
+                <!-- title -->
+                <div >
+                    <h1 class="text-32px font-medium leading-none my-0">{{selectedMovie.title}}</h1>
+                    <h3 class="font-normal text-24px leading-none mt-4px mb-0 ">
+                        <span class="pr-17px">{{ selectedMovie.year }}</span>
+                        <span class="pr-17px">{{ selectedMovie.rated }}</span>
+                        <span class="pr-17px">{{ selectedMovie.runtime }}</span>
+                    </h3>
+                </div>
+                <!-- location -->
+                <div class="h-46px w-954px bg-#030303 rounded-lg flex items-center mt-18px mb-0">
+                    <p class="text-start text-16px font-normal px-24px py-14px ">Turn on Location Services to find cinema theaters nearest you!</p>
+                </div>
+                <!-- cards -->
+                <div>
+                    <p></p>
+                </div>
+            </div>
+       
+      </div>
+
+
+        </div>
     </div>
+
 </template>
 
+
 <script setup>
-definePageMeta({ 
+const {movies} = useMovieData();
+const { id }= useRoute().params
+
+const selectedMovie = movies.find( movie => movie.title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') === id);
+
+const bgStyle = {
+  'background-image': `url('/images/${selectedMovie.posterLink}')`,
+  'background-position': '50% 35%',
+  'background-size': 'cover'
+}
+
+definePageMeta({
     layout: 'movie'
-})      
-    
+})
+
 </script>
 
-<style  scoped>
+<style>
+.format {
+    text-decoration: none;
+    color: white;
+}
+
+.router-link-active {
+    border-bottom: 1px solid white;
+    padding-bottom: 5px;
+    
+}
+
 
 </style>
+
+
+
+
